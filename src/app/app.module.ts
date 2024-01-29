f22177d0496699f74ffdeb91420bebc78a1c572f
Pablo Corrêa <dexpb@hotmail.com>
2024-01-28 23:33:43 -0300

    buzzfeed clone created

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizzComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
